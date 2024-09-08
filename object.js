6997b3a53b7211422f3d516260efe45bbf0e850d
unknown <bhim magar>
2024-09-08 17:17:49 +0545

    first commit

---
+++ object.js
@@ -0,0 +1,15 @@
+const computer = {
+    fname:"dell",
+    color:"black",
+    size:"small"
+}
+console.log(computer.fname+computer.color+"The size of computer"+computer.size);
+ 
+
+// 2nd object 
+const cartoon = {
+    anime1:"horitika",
+anime2:"jenjutsu",
+    anime3:"horimiya"
+}
+console.log("this anime is sukoi" + cartoon.anime1+""+cartoon.anime2);
